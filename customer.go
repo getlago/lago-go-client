36ab24f4b878bae8eda46cafada770600488b5b8
Jeremy Denquin <jdenquin@gmail.com>
2022-07-27 18:09:29 +0200

    use now resty, add first customer func

--- customer.go
+++ customer.go
@@ -1,9 +1,17 @@
 package lago
 
-import "github.com/google/uuid"
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
 
 type CustomerParams struct {
 	Customer CustomerInput `json:"customer"`
+}
+
+type CustomerResult struct {
+	Customer Customer `json:"customer"`
 }
 
 type CustomerInput struct {
@@ -17,4 +25,32 @@ type Customer struct {
 	CustomerID string    `json:"customer_id"`
 	Name       string    `json:"name"`
 	Email      string    `json:"email"`
+}
+
+type CustomerRequest struct {
+	client *Client
+}
+
+func (c *Client) Customer() *CustomerRequest {
+	return &CustomerRequest{
+		client: c,
+	}
+}
+
+func (cr *CustomerRequest) Create(ci *CustomerInput) *Customer {
+	cp := &CustomerParams{
+		Customer: *ci,
+	}
+
+	resp, err := cr.client.HttpClient.R().
+		SetBody(*cp).
+		SetResult(&Customer{}).
+		Post("customers")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(resp)
+
+	return nil
 }
