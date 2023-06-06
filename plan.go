70d5d725dfc00c016ba8d56b3c817fd21ea1c069
Federico Sordillo <fes300@users.noreply.github.com>
2023-06-06 16:16:40 +0200

    feat: add trial period to PlanInput (#94)
    * feat: add trial period to PlanInput
    
    * fix: trial period type
    
    Co-authored-by: Romain Sempé <romain.sempe@gmail.com>
    
    ---------
    
    Co-authored-by: Federico Sordillo <federico@juni.co>
    Co-authored-by: Romain Sempé <romain.sempe@gmail.com>

--- plan.go
+++ plan.go
@@ -51,6 +51,7 @@ type PlanInput struct {
 	AmountCurrency    Currency          `json:"amount_currency,omitempty"`
 	PayInAdvance      bool              `json:"pay_in_advance"`
 	BillChargeMonthly bool              `json:"bill_charge_monthly"`
+	TrialPeriod       float32           `json:"trial_period"`
 	Charges           []PlanChargeInput `json:"charges,omitempty"`
 }
 
