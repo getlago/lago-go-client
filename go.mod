fcfd47d01755bbe9dec3dff44fa35bd2cd7013db
Federico Sordillo <fes300@users.noreply.github.com>
2023-06-08 09:38:26 +0200

    feat: add webhook (#96)
    * feat: add webhook
    
    * feat: switch to non deprecated implementation
    
    * feat: upgrade go
    
    * feat: install golangci-lint from source
    
    * feat: use go version from mod file
    
    ---------
    
    Co-authored-by: Federico Sordillo <federico@juni.co>

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.18
 
 require github.com/google/uuid v1.3.0
 
-require github.com/go-resty/resty/v2 v2.7.0
+require (
+	github.com/go-resty/resty/v2 v2.7.0
+	github.com/golang-jwt/jwt/v5 v5.0.0
+)
 
 require golang.org/x/net v0.7.0 // indirect
---
+++ webhook.go
@@ -0,0 +1,110 @@
+package lago
+
+import (
+	"context"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"net/http"
+
+	jwt "github.com/golang-jwt/jwt/v5"
+)
+
+type WebhookRequest struct {
+	client *Client
+}
+
+func (c *Client) Webhook() *WebhookRequest {
+	return &WebhookRequest{
+		client: c,
+	}
+}
+
+func (wr *WebhookRequest) GetPublicKey(ctx context.Context) (*rsa.PublicKey, *Error) {
+	clientRequest := &ClientRequest{
+		Path: "webhooks/public_key",
+	}
+
+	result, err := wr.client.Get(ctx, clientRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	validatedResult, ok := result.(string)
+	if !ok {
+		return nil, &Error{
+			Err:            errors.New("response is not a string"),
+			HTTPStatusCode: http.StatusInternalServerError,
+			Msg:            "response is not a string",
+		}
+	}
+
+	// Decode the base64-encoded key
+	bytesResult, decodeErr := base64.StdEncoding.DecodeString(validatedResult)
+	if err != nil {
+		return nil, &Error{
+			Err:            decodeErr,
+			HTTPStatusCode: http.StatusInternalServerError,
+			Msg:            "cannot decode the key",
+		}
+	}
+
+	// Parse the PEM block
+	block, _ := pem.Decode(bytesResult)
+	if block == nil || block.Type != "PUBLIC KEY" {
+		return nil, &Error{
+			Err:            errors.New("Failed to decode PEM block containing public key"),
+			HTTPStatusCode: http.StatusInternalServerError,
+			Msg:            "Failed to decode PEM block containing public key",
+		}
+	}
+
+	// Parse the DER-encoded public key
+	publicKey, parseErr := x509.ParsePKIXPublicKey(block.Bytes)
+	if parseErr != nil {
+		return nil, &Error{
+			Err:            parseErr,
+			HTTPStatusCode: http.StatusInternalServerError,
+			Msg:            "Failed to to parse the public key",
+		}
+	}
+
+	rsaPublicKey, ok := publicKey.(*rsa.PublicKey)
+	if !ok {
+		return nil, &Error{
+			Err:            errors.New("Unexpected type of public key"),
+			HTTPStatusCode: http.StatusInternalServerError,
+			Msg:            "Unexpected type of public key",
+		}
+	}
+
+	return rsaPublicKey, nil
+}
+
+func (wr *WebhookRequest) ValidateSignature(ctx context.Context, signature string) (bool, *Error) {
+	publicKey, err := wr.GetPublicKey(ctx)
+	if err != nil {
+		return false, err
+	}
+
+	token, parseErr := jwt.Parse(signature, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return publicKey, nil
+	})
+	if parseErr != nil {
+		return false, &Error{
+			Err:            parseErr,
+			HTTPStatusCode: http.StatusInternalServerError,
+			Msg:            "cannot parse token",
+		}
+	}
+
+	println(fmt.Printf("token: %+v", token))
+
+	return token.Valid, nil
+}
