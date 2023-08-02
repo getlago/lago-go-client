64ae7359aecbff2a01e729000a2a708a69406b07
Jeremy Denquin <jeremy@getlago.com>
2023-08-02 15:59:19 +0200

    feat(customers): Add updatedAt field

--- customer.go
+++ customer.go
@@ -150,6 +150,7 @@ type Customer struct {
 	Taxes []Tax `json:"taxes,omitempty"`
 
 	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
 }
 
 type CustomerRequest struct {
