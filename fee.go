679bb1de43061e02bbc79de35a32877e17a0513a
Vincent Pochet <vincent@getlago.com>
2024-02-12 09:45:11 +0100

    feat(grouped-by): Add new fields (#159)

--- fee.go
+++ fee.go
@@ -77,13 +77,14 @@ type FeeListInput struct {
 }
 
 type FeeItem struct {
-	Type                    FeeType     `json:"type,omitempty"`
-	Code                    string      `json:"code,omitempty"`
-	Name                    string      `json:"name,omitempty"`
-	InvoiceDisplayName      string      `json:"invoice_display_name,omitempty"`
-	GroupInvoiceDisplayName string      `json:"group_invoice_display_name,omitempty"`
-	LagoItemID              uuid.UUID   `json:"lago_item_id,omitempty"`
-	ItemType                FeeItemType `json:"item_type,omitempty"`
+	Type                    FeeType                `json:"type,omitempty"`
+	Code                    string                 `json:"code,omitempty"`
+	Name                    string                 `json:"name,omitempty"`
+	InvoiceDisplayName      string                 `json:"invoice_display_name,omitempty"`
+	GroupInvoiceDisplayName string                 `json:"group_invoice_display_name,omitempty"`
+	LagoItemID              uuid.UUID              `json:"lago_item_id,omitempty"`
+	ItemType                FeeItemType            `json:"item_type,omitempty"`
+	GroupedBy               map[string]interface{} `json:"grouped_by,omitempty"`
 }
 
 type FeeAppliedTax struct {
