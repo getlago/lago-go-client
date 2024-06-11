b360b606dcd23aa4c7c6e162d4d7bb34522a489f
Jeroen Bobbeldijk <jeroen@klippa.com>
2024-06-11 13:41:38 +0200

    Add support for multiple errors

--- error.go
+++ error.go
@@ -19,6 +19,62 @@ var ErrorTypeAssert = Error{
 	Message:        "Type assertion failed",
 }
 
+type ErrorDetail struct {
+	Multiple bool
+	Errors   map[int]map[string][]string
+}
+
+func (ed *ErrorDetail) DetailsForRow(row int) (map[string][]string, error) {
+	if !ed.Multiple {
+		return nil, errors.New("error contains a single error, use Error()")
+	}
+
+	rowDetail, ok := ed.Errors[row]
+	if !ok {
+		return nil, nil
+	}
+
+	return rowDetail, nil
+}
+
+func (ed *ErrorDetail) Details() (map[string][]string, error) {
+	if ed.Multiple {
+		return nil, errors.New("error contains multiple errors, use ErrorForRow()")
+	}
+
+	return ed.Errors[0], nil
+}
+
+func (ed *ErrorDetail) UnmarshalJSON(data []byte) error {
+	// First attempt to unmarshal singular.
+	var singularErr map[string][]string
+	err := json.Unmarshal(data, &singularErr)
+	if err == nil {
+		ed.Errors = map[int]map[string][]string{
+			0: singularErr,
+		}
+		return nil
+	}
+
+	// Then attempt to unmarshal multiple.
+	var multipleErr map[int]map[string][]string
+	err = json.Unmarshal(data, &multipleErr)
+	if err == nil {
+		ed.Errors = multipleErr
+		ed.Multiple = true
+		return nil
+	}
+
+	return err
+}
+
+func (ed *ErrorDetail) MarshalJSON() ([]byte, error) {
+	if ed.Multiple {
+		return json.Marshal(ed.Errors)
+	}
+	return json.Marshal(ed.Errors[0])
+}
+
 type Error struct {
 	Err error `json:"-"`
 
@@ -26,7 +82,7 @@ type Error struct {
 	Message        string `json:"error"`
 	ErrorCode      string `json:"code"`
 
-	ErrorDetail map[string][]string `json:"error_details,omitempty"`
+	ErrorDetail *ErrorDetail `json:"error_details,omitempty"`
 }
 
 func (e Error) Error() string {
