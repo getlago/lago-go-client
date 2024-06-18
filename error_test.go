ba07d56bb3aef9429ed61d517db8b0b215703d12
jerbob92 <jerbob92@users.noreply.github.com>
2024-06-18 09:17:34 +0200

    Add support for multiple errors (#181)

--- error_test.go
+++ error_test.go
@@ -1,6 +1,7 @@
 package lago
 
 import (
+	"encoding/json"
 	"errors"
 	"testing"
 )
@@ -20,4 +21,118 @@ func TestErrorNoErr(t *testing.T) {
 		Message:        "500",
 	}
 	t.Logf("%s", noErr.Error())
+}
+
+func TestErrorDetails(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input string
+		want  *Error
+	}{
+		{
+			name: "Single detail",
+			input: `{
+  "status": 422,
+  "error": "Unprocessable Entity",
+  "code": "validation_errors",
+  "error_details": {
+    "transaction_id": [
+      "value_already_exist"
+    ]
+  }
+}`,
+			want: &Error{
+				HTTPStatusCode: 422,
+				Message:        "Unprocessable Entity",
+				ErrorCode:      "validation_errors",
+				ErrorDetail: &ErrorDetail{
+					Multiple: false,
+					Errors: map[int]map[string][]string{
+						0: {
+							"transaction_id": {
+								"value_already_exist",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name: "Multiple details",
+			input: `{
+  "status": 422,
+  "error": "Unprocessable Entity",
+  "code": "validation_errors",
+  "error_details": {
+    "0": {
+      "transaction_id": [
+        "value_already_exist"
+      ]
+    },
+    "1": {
+      "transaction_id": [
+        "value_already_exist"
+      ]
+    },
+    "2": {
+      "transaction_id": [
+        "value_already_exist"
+      ]
+    }
+  }
+}`,
+			want: &Error{
+				HTTPStatusCode: 422,
+				Message:        "Unprocessable Entity",
+				ErrorCode:      "validation_errors",
+				ErrorDetail: &ErrorDetail{
+					Multiple: true,
+					Errors: map[int]map[string][]string{
+						0: {
+							"transaction_id": {
+								"value_already_exist",
+							},
+						},
+						1: {
+							"transaction_id": {
+								"value_already_exist",
+							},
+						},
+						2: {
+							"transaction_id": {
+								"value_already_exist",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			errObj := &Error{}
+			err := json.Unmarshal([]byte(tt.input), errObj)
+			if err != nil {
+				t.Errorf("got error %s", err.Error())
+				return
+			}
+
+			expectErr, err := json.Marshal(tt.want)
+			if err != nil {
+				t.Errorf("got error %s", err.Error())
+				return
+			}
+
+			gotErr, err := json.Marshal(errObj)
+			if err != nil {
+				t.Errorf("got error %s", err.Error())
+				return
+			}
+
+			if string(expectErr) != string(gotErr) {
+				t.Errorf("got error %s, but expected error %s", string(gotErr), string(expectErr))
+				return
+			}
+		})
+	}
 }
