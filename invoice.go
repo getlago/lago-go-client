931c4b6ce26cb5535b05aef3fe2a6a700a44dcf1
Anna Velentsevich <annvelents@gmail.com>
2024-07-24 14:46:03 +0200

    feat(anrok): add error details to invoie response

--- invoice.go
+++ invoice.go
@@ -142,6 +142,12 @@ type InvoiceAppliedTax struct {
 	CreatedAt       time.Time `json:"created_at,omitempty"`
 }
 
+type InvoiceErrorDetail struct {
+    LagoId          uuid.UUID `json:"lago_id,omitempty"`
+    ErrorCode       string    `json:"error_code,omitempty"`
+    ErrorDetails    string    `json:"error_details,omitempty"`
+}
+
 type Invoice struct {
 	LagoID       uuid.UUID `json:"lago_id,omitempty"`
 	SequentialID int       `json:"sequential_id,omitempty"`
@@ -175,9 +181,10 @@ type Invoice struct {
 	Customer      *Customer      `json:"customer,omitempty"`
 	Subscriptions []Subscription `json:"subscriptions,omitempty"`
 
-	Fees         []Fee               `json:"fees,omitempty"`
-	Credits      []InvoiceCredit     `json:"credits,omitempty"`
-	AppliedTaxes []InvoiceAppliedTax `json:"applied_taxes,omitempty"`
+	Fees         []Fee                `json:"fees,omitempty"`
+	ErrorDetails []InvoiceErrorDetail `json:"error_details,omitempty"`
+	Credits      []InvoiceCredit      `json:"credits,omitempty"`
+	AppliedTaxes []InvoiceAppliedTax  `json:"applied_taxes,omitempty"`
 }
 
 type InvoicePaymentUrl struct {
