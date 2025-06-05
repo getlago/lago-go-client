fde6b39208fbad7eb598f0892fbd61c9f40123f0
Yohan Robert <yohan@getlago.com>
2025-06-05 18:58:52 +0200

    Test new workflow

--- billing_entity.go
+++ billing_entity.go
@@ -11,7 +11,7 @@ import (
 type BillingEntityDocumentNumbering string
 
 const (
-	BillingEntityDocumentNumberingPerCustomer      BillingEntityDocumentNumbering = "per_customer"
+	BillingEntityDocumentNumberingPerCustomer     BillingEntityDocumentNumbering = "per_customer"
 	BillingEntityDocumentNumberingPerBillingEntity BillingEntityDocumentNumbering = "per_billing_entity"
 )
 
@@ -28,9 +28,9 @@ type BillingEntityUpdateParams struct {
 }
 
 type BillingEntityResult struct {
-	BillingEntity   *BillingEntity  `json:"billing_entity,omitempty"`
-	BillingEntities []BillingEntity `json:"billing_entities,omitempty"`
-	Meta            Metadata        `json:"meta,omitempty"`
+	BillingEntity   *BillingEntity   `json:"billing_entity,omitempty"`
+	BillingEntities []BillingEntity  `json:"billing_entities,omitempty"`
+	Meta            Metadata         `json:"meta,omitempty"`
 }
 
 type BillingEntityBillingConfiguration struct {
@@ -66,59 +66,59 @@ type BillingEntity struct {
 	EuTaxManagement           bool                           `json:"eu_tax_management,omitempty"`
 	LogoURL                   string                         `json:"logo_url,omitempty"`
 	InvoiceFooter             string                         `json:"invoice_footer,omitempty"`
-	InvoiceGracePeriod        int                            `json:"invoice_grace_period,omitempty"`
-	DocumentLocale            string                         `json:"document_locale,omitempty"`
-	Taxes                     []Tax                          `json:"taxes,omitempty"`
+    InvoiceGracePeriod        int                            `json:"invoice_grace_period,omitempty"`
+    DocumentLocale            string                         `json:"document_locale,omitempty"`
+	Taxes 					  []Tax 					     `json:"taxes,omitempty"`
 }
 
-type BillingEntityCreateInput struct {
-	Name                      string                            `json:"name,omitempty"`
-	Code                      string                            `json:"code,omitempty"`
-	Email                     string                            `json:"email,omitempty"`
-	AddressLine1              string                            `json:"address_line1,omitempty"`
-	AddressLine2              string                            `json:"address_line2,omitempty"`
-	City                      string                            `json:"city,omitempty"`
-	Zipcode                   string                            `json:"zipcode,omitempty"`
-	State                     string                            `json:"state,omitempty"`
-	Country                   string                            `json:"country,omitempty"`
-	DefaultCurrency           Currency                          `json:"default_currency,omitempty"`
-	LegalName                 string                            `json:"legal_name,omitempty"`
-	LegalNumber               string                            `json:"legal_number,omitempty"`
-	DocumentNumbering         BillingEntityDocumentNumbering    `json:"document_numbering,omitempty"`
-	DocumentNumberPrefix      string                            `json:"document_number_prefix,omitempty"`
-	NetPaymentTerm            int                               `json:"net_payment_term,omitempty"`
-	Timezone                  string                            `json:"timezone,omitempty"`
-	EmailSettings             []string                          `json:"email_settings,omitempty"`
-	TaxIdentificationNumber   string                            `json:"tax_identification_number,omitempty"`
-	FinalizeZeroAmountInvoice bool                              `json:"finalize_zero_amount_invoice,omitempty"`
-	EuTaxManagement           bool                              `json:"eu_tax_management,omitempty"`
-	BillingConfiguration      BillingEntityBillingConfiguration `json:"billing_configuration,omitempty"`
-	LogoBase64                string                            `json:"logo,omitempty"`
+type BillingEntityCreateInput struct {			
+	Name                     	string                         			`json:"name,omitempty"`
+	Code                     	string                         			`json:"code,omitempty"`
+	Email                    	string                         			`json:"email,omitempty"`
+	AddressLine1             	string                         			`json:"address_line1,omitempty"`
+	AddressLine2             	string                         			`json:"address_line2,omitempty"`
+	City                     	string                         			`json:"city,omitempty"`
+	Zipcode                  	string                         			`json:"zipcode,omitempty"`
+	State                    	string                         			`json:"state,omitempty"`
+	Country                  	string                         			`json:"country,omitempty"`
+	DefaultCurrency          	Currency                       			`json:"default_currency,omitempty"`
+	LegalName                	string                         			`json:"legal_name,omitempty"`
+	LegalNumber              	string                         			`json:"legal_number,omitempty"`
+	DocumentNumbering        	BillingEntityDocumentNumbering 			`json:"document_numbering,omitempty"`
+	DocumentNumberPrefix     	string                         			`json:"document_number_prefix,omitempty"`
+	NetPaymentTerm           	int                            			`json:"net_payment_term,omitempty"`
+	Timezone                 	string                         			`json:"timezone,omitempty"`
+	EmailSettings            	[]string                       			`json:"email_settings,omitempty"`
+	TaxIdentificationNumber  	string                         			`json:"tax_identification_number,omitempty"`
+	FinalizeZeroAmountInvoice 	bool                           			`json:"finalize_zero_amount_invoice,omitempty"`
+	EuTaxManagement          	bool                           			`json:"eu_tax_management,omitempty"`
+	BillingConfiguration 	  	BillingEntityBillingConfiguration 		`json:"billing_configuration,omitempty"`
+	LogoBase64                	string                         			`json:"logo,omitempty"`
 }
 
 type BillingEntityUpdateInput struct {
-	Name                      string                            `json:"name,omitempty"`
-	Email                     string                            `json:"email,omitempty"`
-	AddressLine1              string                            `json:"address_line1,omitempty"`
-	AddressLine2              string                            `json:"address_line2,omitempty"`
-	City                      string                            `json:"city,omitempty"`
-	Zipcode                   string                            `json:"zipcode,omitempty"`
-	State                     string                            `json:"state,omitempty"`
-	Country                   string                            `json:"country,omitempty"`
-	DefaultCurrency           Currency                          `json:"default_currency,omitempty"`
-	LegalName                 string                            `json:"legal_name,omitempty"`
-	LegalNumber               string                            `json:"legal_number,omitempty"`
-	DocumentNumbering         BillingEntityDocumentNumbering    `json:"document_numbering,omitempty"`
-	DocumentNumberPrefix      string                            `json:"document_number_prefix,omitempty"`
-	NetPaymentTerm            int                               `json:"net_payment_term,omitempty"`
-	Timezone                  string                            `json:"timezone,omitempty"`
-	EmailSettings             []string                          `json:"email_settings,omitempty"`
-	TaxIdentificationNumber   string                            `json:"tax_identification_number,omitempty"`
-	FinalizeZeroAmountInvoice bool                              `json:"finalize_zero_amount_invoice,omitempty"`
-	EuTaxManagement           bool                              `json:"eu_tax_management,omitempty"`
-	BillingConfiguration      BillingEntityBillingConfiguration `json:"billing_configuration,omitempty"`
-	LogoBase64                string                            `json:"logo,omitempty"`
-	TaxCodes                  []string                          `json:"tax_codes,omitempty"`
+	Name                     	string                         			`json:"name,omitempty"`
+	Email                    	string                         			`json:"email,omitempty"`
+	AddressLine1             	string                         			`json:"address_line1,omitempty"`
+	AddressLine2             	string                         			`json:"address_line2,omitempty"`
+	City                     	string                         			`json:"city,omitempty"`
+	Zipcode                  	string                         			`json:"zipcode,omitempty"`
+	State                    	string                         			`json:"state,omitempty"`
+	Country                  	string                         			`json:"country,omitempty"`
+	DefaultCurrency          	Currency                       			`json:"default_currency,omitempty"`
+	LegalName                	string                         			`json:"legal_name,omitempty"`
+	LegalNumber              	string                         			`json:"legal_number,omitempty"`
+	DocumentNumbering        	BillingEntityDocumentNumbering 			`json:"document_numbering,omitempty"`
+	DocumentNumberPrefix     	string                         			`json:"document_number_prefix,omitempty"`
+	NetPaymentTerm           	int                            			`json:"net_payment_term,omitempty"`
+	Timezone                 	string                         			`json:"timezone,omitempty"`
+	EmailSettings            	[]string                       			`json:"email_settings,omitempty"`
+	TaxIdentificationNumber  	string                         			`json:"tax_identification_number,omitempty"`
+	FinalizeZeroAmountInvoice 	bool                           			`json:"finalize_zero_amount_invoice,omitempty"`
+	EuTaxManagement          	bool                           			`json:"eu_tax_management,omitempty"`
+	BillingConfiguration 	  	BillingEntityBillingConfiguration 		`json:"billing_configuration,omitempty"`
+	LogoBase64                	string                         			`json:"logo,omitempty"`
+	TaxCodes                    []string                       			`json:"tax_codes,omitempty"`
 }
 
 func (c *Client) BillingEntity() *BillingEntityRequest {
