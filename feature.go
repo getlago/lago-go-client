1f4d765aad530ab743aca7861b3058a810e050c7
Julien Bourdeau <julien@julienbourdeau.com>
2025-08-27 11:18:23 +0200

    Add Feature API

---
+++ feature.go
@@ -0,0 +1,196 @@
+package lago
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+)
+
+type FeatureRequest struct {
+	client *Client
+}
+
+type Privilege struct {
+	Code      string `json:"code"`
+	Name      string `json:"name,omitempty"`
+	ValueType string `json:"value_type"` // TODO: add enum
+	// TODO: Add config
+}
+
+type Feature struct {
+	Name        string      `json:"name,omitempty"`
+	Code        string      `json:"code"`
+	Description string      `json:"description,omitempty"`
+	Privileges  []Privilege `json:"privileges,omitempty"`
+}
+
+type FeatureResult struct {
+	Feature  *Feature  `json:"feature,omitempty"`
+	Features []Feature `json:"features,omitempty"`
+	Meta     Metadata  `json:"meta,omitempty"`
+}
+
+type FeatureListInput struct {
+	PerPage int `json:"per_page,omitempty,string"`
+	Page    int `json:"page,omitempty,string"`
+}
+
+type FeatureParams struct {
+	FeatureInput *FeatureInput `json:"feature,omitempty"`
+}
+
+type FeatureInput struct {
+	Name        string           `json:"name,omitempty"`
+	Code        string           `json:"code,omitempty"`
+	Description string           `json:"description,omitempty"`
+	Privileges  []PrivilegeInput `json:"privileges,omitempty"`
+}
+
+type PrivilegeInput struct {
+	Code      string `json:"code,omitempty"`
+	Name      string `json:"name,omitempty"`
+	ValueType string `json:"value_type,omitempty"`
+	// TODO: Add config
+}
+
+func (c *Client) Feature() *FeatureRequest {
+	return &FeatureRequest{
+		client: c,
+	}
+}
+
+func (bmr *FeatureRequest) Get(ctx context.Context, featureCode string) (*Feature, *Error) {
+	subPath := fmt.Sprintf("%s/%s", "features", featureCode)
+	clientRequest := &ClientRequest{
+		Path:   subPath,
+		Result: &FeatureResult{},
+	}
+
+	result, err := bmr.client.Get(ctx, clientRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	featureResult, ok := result.(*FeatureResult)
+	if !ok {
+		return nil, &ErrorTypeAssert
+	}
+
+	return featureResult.Feature, nil
+}
+
+func (bmr *FeatureRequest) GetList(ctx context.Context, featureListInput *FeatureListInput) (*FeatureResult, *Error) {
+	jsonQueryParams, err := json.Marshal(featureListInput)
+	if err != nil {
+		return nil, &Error{Err: err}
+	}
+
+	queryParams := make(map[string]string)
+	if err = json.Unmarshal(jsonQueryParams, &queryParams); err != nil {
+		return nil, &Error{Err: err}
+	}
+
+	clientRequest := &ClientRequest{
+		Path:        "features",
+		QueryParams: queryParams,
+		Result:      &FeatureResult{},
+	}
+
+	result, clientErr := bmr.client.Get(ctx, clientRequest)
+	if clientErr != nil {
+		return nil, clientErr
+	}
+
+	featureResult, ok := result.(*FeatureResult)
+	if !ok {
+		return nil, &ErrorTypeAssert
+	}
+
+	return featureResult, nil
+}
+
+func (bmr *FeatureRequest) Create(ctx context.Context, featureInput *FeatureInput) (*Feature, *Error) {
+
+	clientRequest := &ClientRequest{
+		Path:   "features",
+		Result: &FeatureResult{},
+		Body: &FeatureParams{
+			FeatureInput: featureInput,
+		},
+	}
+
+	result, err := bmr.client.Post(ctx, clientRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	featureResult, ok := result.(*FeatureResult)
+	if !ok {
+		return nil, &ErrorTypeAssert
+	}
+
+	return featureResult.Feature, nil
+}
+
+func (bmr *FeatureRequest) Update(ctx context.Context, featureInput *FeatureInput) (*Feature, *Error) {
+	subPath := fmt.Sprintf("%s/%s", "features", featureInput.Code)
+	clientRequest := &ClientRequest{
+		Path:   subPath,
+		Result: &FeatureResult{},
+		Body: &FeatureParams{
+			FeatureInput: featureInput,
+		},
+	}
+
+	result, err := bmr.client.Put(ctx, clientRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	featureResult, ok := result.(*FeatureResult)
+	if !ok {
+		return nil, &ErrorTypeAssert
+	}
+
+	return featureResult.Feature, nil
+}
+
+func (bmr *FeatureRequest) Delete(ctx context.Context, featureCode string) (*Feature, *Error) {
+	subPath := fmt.Sprintf("%s/%s", "features", featureCode)
+	clientRequest := &ClientRequest{
+		Path:   subPath,
+		Result: &FeatureResult{},
+	}
+
+	result, err := bmr.client.Delete(ctx, clientRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	featureResult, ok := result.(*FeatureResult)
+	if !ok {
+		return nil, &ErrorTypeAssert
+	}
+
+	return featureResult.Feature, nil
+}
+
+func (bmr *FeatureRequest) DeletePrivilege(ctx context.Context, featureCode string, privilegeCode string) (*Feature, *Error) {
+	subPath := fmt.Sprintf("%s/%s/%s/%s", "features", featureCode, "privileges", privilegeCode)
+	clientRequest := &ClientRequest{
+		Path:   subPath,
+		Result: &FeatureResult{},
+	}
+
+	result, err := bmr.client.Delete(ctx, clientRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	featureResult, ok := result.(*FeatureResult)
+	if !ok {
+		return nil, &ErrorTypeAssert
+	}
+
+	return featureResult.Feature, nil
+}
