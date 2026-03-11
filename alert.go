2e23565c15ed487d9507f8e3391f691c58c823a0
Anna Velentsevich <annvelents@gmail.com>
2026-03-11 10:46:00 +0100

    move statusQueryParams to utils

--- alert.go
+++ alert.go
@@ -232,11 +232,3 @@ func (ar *AlertRequest) DeleteAll(ctx context.Context, subscriptionExternalID st
 
 	return nil
 }
-
-// statusQueryParams builds a query param map from an optional status variadic arg.
-func statusQueryParams(subscriptionStatus []string) map[string]string {
-	if len(subscriptionStatus) > 0 && subscriptionStatus[0] != "" {
-		return map[string]string{"subscription_status": subscriptionStatus[0]}
-	}
-	return nil
-}
